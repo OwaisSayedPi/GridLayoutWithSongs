d65f6d45c42a1af14759c2c8616c0d0502e7a64d
Owais.Sayed <Owais.Sayed@PC-33>
2023-06-26 18:07:59 +0530

    Add project files.

---
+++ GridLayoutWithSongs/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GridLayoutWithSongs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public static List<Songs> GenerateSongList()
+        {
+            List<Songs> songs = new List<Songs>();
+            Random random = new Random();
+            for (int i = 1; i <= 10; i++)
+            {
+                Songs song = new Songs();
+                song.SongID = i;
+                song.SongRating = random.Next(5);                
+                song.SongName = "AAAAAAAA";
+                song.MaxRatingLimit = 5;
+                songs.Add(song);
+            }
+            return songs;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<Songs> songs = GenerateSongList();
+            MainGrid.ItemsSource = songs;            
+        }
+    }
+}
---
+++ GridLayoutWithSongs/Songs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridLayoutWithSongs
+{
+    public class Songs
+    {
+        public int SongID { get; set; }
+        public string SongName { get; set; }
+        public int SongRating { get; set; }
+        public int MaxRatingLimit { get; set; }
+        private string displayHearts;
+
+        public string DisplayHearts
+        {
+            get {
+                displayHearts = "♥".Repeat(SongRating, MaxRatingLimit);
+                return displayHearts; }
+        }        
+    }
+    static class ExtensionClass
+    {
+        public static string Repeat(this string toRepeat, int SongRating, int Limit)
+        {
+            string temp = "";
+            for (int x = 1; x <= SongRating; x++)
+            {
+                temp += toRepeat;
+            }
+            for (int i = 0; i < Limit-SongRating; i++)
+            {
+                temp += "-";
+            }
+            return temp;
+        }
+    }
+}
